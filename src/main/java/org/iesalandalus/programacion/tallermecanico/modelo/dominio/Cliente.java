3ea4acfef0d1ad6f91dbad08db5f66bedb16a017
Tony Gomez Muriel <agmuriel93@gmail.com>
2025-02-26 11:07:08 +0100

    Commit inicial con el enunciado, las dependencias y los tests.

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Cliente.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Cliente.java
@@ -30,7 +30,10 @@ public class Cliente {
     }
 
     public void setNombre(String nombre) {
-        Objects.requireNonNull(cliente, "")
+        Objects.requireNonNull(nombre, "El nombre no puede ser nulo.");
+        if (!nombre.matches(ER_NOMBRE)) {
+            throw new IllegalArgumentException("El nombre no tiene un formato válido.");
+        }
         this.nombre = nombre;
     }
 
@@ -39,19 +42,21 @@ public class Cliente {
     }
 
     private void setDni(String dni) {
-
-        if(dni.matches(ER_DNI)) {
-            this.dni = dni;
+        Objects.requireNonNull(dni,"El DNI no puede ser nulo.");
+        if(!dni.matches(ER_DNI)) {
+            throw new IllegalArgumentException("El DNI no tiene un formato válido.");
         }
-
+        if(!comprobarLetraDni(dni)) {
+            throw new IllegalArgumentException("La letra del DNI no es correcta.");
+        }
+        this.dni = dni;
     }
 
     private boolean comprobarLetraDni(String dni) {
-
-        setDni();
-
-        return true;
-
+        String letrasDNi = "TRWAGMYFPDXBNJZSQVHLCKE";
+        int numero = Integer.parseInt(dni.substring(0,8));
+        char letra = dni.charAt(8);
+        return letra == letrasDNi.charAt(numero % 23);
     }
 
     public String getTelefono() {
@@ -59,20 +64,23 @@ public class Cliente {
     }
 
     public void setTelefono(String telefono) {
+        Objects.requireNonNull(telefono, "El teléfono no puede ser nulo.");
+        if (!telefono.matches(ER_TELEFONO)) {
+            throw new IllegalArgumentException("El teléfono no tiene un formato válido.");
+        }
         this.telefono = telefono;
     }
 
     public static Cliente get(String dni) {
 
-        return new Cliente("pepe", dni , "789123456");
+        return new Cliente("Patricio Estrella", dni , "950111111");
 
 
     }
 
-
     @Override
     public String toString() {
-        return String.format("[nombre=%s, dni=%s, telefono=%s]", nombre, dni, telefono);
+        return String.format("%s - %s (%s)", nombre, dni, telefono);
     }
 
     @Override
--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Revision.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Revision.java
@@ -4,28 +4,33 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
-/*public class Revision {
+public class Revision {
 
-    private static float PRECIO_HORA;
-    private static float PRECIO_DIA;
-    private static float PRECIO_MATERIAL;
-    public static DateTimeFormatter FORMATO_FECHA;
+    private static final float PRECIO_HORA = 30F;
+    private static final float PRECIO_DIA = 10F;
+    private static final float PRECIO_MATERIAL = 1.5F;
+    static final  DateTimeFormatter FORMATO_FECHA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private Cliente cliente;
+    private Vehiculo vehiculo;
     private LocalDate fechaInicio;
     private LocalDate fechaFin;
     private int horas;
     private float precioMaterial;
 
     public Revision(Cliente cliente, Vehiculo vehiculo, LocalDate fechaInicio) {
-
         setCliente(cliente);
         setVehiculo(vehiculo);
         setFechaInicio(fechaInicio);
+        fechaFin = null;
+        horas = 0;
+        precioMaterial = 0;
 
     }
 
     public Revision(Revision revision) {
-
         Objects.requireNonNull(revision,"La revision no puede ser nula");
+        cliente = new Cliente(revision.cliente);
+        vehiculo = revision.vehiculo;
         fechaInicio = revision.fechaInicio;
         fechaFin = revision.fechaFin;
         horas = revision.horas;
@@ -33,69 +38,72 @@ import java.util.Objects;
     }
 
     public Cliente getCliente() {
-
-        return getCliente();
-
+        return cliente;
     }
 
     private void setCliente(Cliente cliente) {
-
+        Objects.requireNonNull(cliente, "El cliente no puede ser nulo.");
         this.cliente = cliente;
     }
 
     public Vehiculo getVehiculo() {
-
         return getVehiculo();
-
     }
 
     private void setVehiculo( Vehiculo vehiculo) {
-
+        Objects.requireNonNull(vehiculo, "El vehiculo no puede ser nulo.");
         this.vehiculo = vehiculo;
-
     }
 
     public LocalDate getFechaInicio() {
-
         return getFechaInicio();
-
     }
 
     private void setFechaInicio(LocalDate fechaInicio) {
+        Objects.requireNonNull(fechaInicio, "La fecha de inicio no puede ser nula.");
+        if(fechaInicio.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException("la fecha de inicio no puede ser futura.");
+        }
 
         this.fechaInicio = fechaInicio;
-
     }
 
     public LocalDate getFechaFin() {
-
         return getFechaFin();
-
     }
 
     private void setFechaFin(LocalDate fechaFin) {
-
+        Objects.requireNonNull(fechaFin, "La fecha de fin no puede ser nula.");
+        if(fechaFin.isBefore(fechaInicio)) {
+            throw new IllegalArgumentException("La fecha fin no puede ser anterior a la fecha de inicio.");
+        }
+        if(fechaFin.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException("La fecha de fin no puede ser futura.");
+        }
         this.fechaFin = fechaFin;
 
     }
 
     public int getHoras() {
-
         return getHoras();
     }
 
-    public void añadirHoras(int horas) {
-
-
+    public void añadirHoras(int horas) throws TallerMecanicoExcepcion {
+        if(horas <= 0) {
+            throw new IllegalArgumentException("Las horas a añadir deben ser mayores que cero.");
+        }
+        if(estaCerrada()) {
+            throw new TallerMecanicoExcepcion("No se puede añadir horas, ya que la revision está cerrada.");
+        }
+        this.horas = horas;
 
     }
 
     public float getPrecioMaterial() {
-
         return getPrecioMaterial();
     }
 
-    public void añadirPrecioMaterial(float precioMaterial) {
+    public void añadirPrecioMaterial(float precioMaterial) throws
 
     }
 
@@ -135,4 +143,3 @@ import java.util.Objects;
         return String.format("[fechaInicio=%s, fechaFin=%s, horas=%s, precioMaterial=%s]", fechaInicio, fechaFin, horas, precioMaterial);
     }
 }
-*/
