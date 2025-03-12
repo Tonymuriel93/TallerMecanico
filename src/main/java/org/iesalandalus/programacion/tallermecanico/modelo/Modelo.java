c00270e9b4d1980ebe86c8c4675905c0884de451
Tony Gomez Muriel <agmuriel93@gmail.com>
2025-03-12 11:13:43 +0100

    Commit inicial con el enunciado, las dependencias y los tests.

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/Modelo.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/Modelo.java
@@ -7,6 +7,9 @@ import org.iesalandalus.programacion.tallermecanico.modelo.negocio.Clientes;
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.Revisiones;
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.Vehiculos;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Modelo {
@@ -27,31 +30,102 @@ public class Modelo {
     public void terminar() {
         System.out.println("Modelo terminado.");
     }
+
     public void insertar(Cliente cliente) throws TallerMecanicoExcepcion {
         clientes.insertar((new Cliente(cliente)));
     }
+
     public void insertar(Vehiculo vehiculo) throws TallerMecanicoExcepcion {
         vehiculos.insertar(vehiculo);
     }
+
     public void insertar(Revision revision) throws TallerMecanicoExcepcion {
         Cliente cliente = clientes.buscar(revision.getCliente());
         Vehiculo vehiculo = vehiculos.buscar(revision.getVehiculo());
         revisiones.insertar(new Revision(cliente, vehiculo, revision.getFechaInicio()));
     }
+
     public Cliente buscar(Cliente cliente) {
         cliente = Objects.requireNonNull(clientes.buscar(cliente), "No existe un cliente igual.");
         return new Cliente(cliente);
     }
-    public  Vehiculo buscar(Vehiculo vehiculo) {
+
+    public Vehiculo buscar(Vehiculo vehiculo) {
         vehiculo = Objects.requireNonNull(vehiculos.buscar(vehiculo), "No existe un vehiculo igual.");
         return vehiculo;
     }
+
     public Revision buscar(Revision revision) {
         revision = Objects.requireNonNull(revisiones.buscar(revision), "No existe una revisión igual.");
         return new Revision(revision);
     }
-    public Cliente modificar(Cliente cliente,String nombre, String telefono) throws TallerMecanicoExcepcion {
+
+    public Cliente modificar(Cliente cliente, String nombre, String telefono) throws TallerMecanicoExcepcion {
         return new Cliente(clientes.modificar(cliente, nombre, telefono));
     }
-    public
+
+    public Revision anadirHoras(Revision revision, int horas) throws TallerMecanicoExcepcion {
+        return new Revision(revisiones.anadirHoras(revision, horas));
+    }
+
+    public Revision anadirPrecioMaterial(Revision revision, float precioMaterial) throws TallerMecanicoExcepcion {
+        return new Revision(revisiones.anadirPrecioMaterial(revision, precioMaterial));
+    }
+
+    public Revision cerrar(Revision revision, LocalDate fechaFin) throws TallerMecanicoExcepcion {
+        return new Revision(revisiones.cerrar(revision, fechaFin));
+    }
+
+    public void borrar(Cliente cliente) throws TallerMecanicoExcepcion {
+        List<Revision> revisionesCliente = revisiones.get(cliente);
+        for (Revision revision : revisionesCliente) {
+            revisiones.borrar(revision);
+        }
+        clientes.borrar(cliente);
+    }
+
+    public void borrar(Vehiculo vehiculo) throws TallerMecanicoExcepcion {
+        List<Revision> revisionesVehiculo = revisiones.get(vehiculo);
+        for (Revision revision : revisionesVehiculo) {
+            revisiones.borrar(revision);
+        }
+        vehiculos.borrar(vehiculo);
+    }
+
+    public void borrar(Revision revision) throws TallerMecanicoExcepcion {
+        revisiones.borrar(revision);
+    }
+
+    public List<Cliente> getClientes() {
+        List<Cliente> copiaClientes = new ArrayList<>();
+        for(Cliente cliente : clientes.get()) {
+            copiaClientes.add(new Cliente(cliente));
+        }
+        return copiaClientes;
+    }
+    public List<Vehiculo> getVehiculos() {
+        return vehiculos.get();
+    }
+    public List<Revision> getRevisiones() {
+        List<Revision> copiaRevisiones = new ArrayList<>();
+        for (Revision revision : revisiones.get()) {
+            copiaRevisiones.add(new Revision(revision));
+        }
+        return copiaRevisiones;
+    }
+    public List<Revision> getRevisiones(Cliente cliente) {
+        List<Revision> revisionesCliente = new ArrayList<>();
+        for (Revision revision : revisiones.get(cliente)) {
+            revisionesCliente.add(new Revision(revision));
+        }
+        return revisionesCliente;
+    }
+    public List<Revision> getRevisiones(Vehiculo vehiculo) {
+        List<Revision> revisionesVehiculo = new ArrayList<>();
+        for (Revision revision : revisiones.get(vehiculo)) {
+            revisionesVehiculo.add(new Revision(revision));
+        }
+        return revisionesVehiculo;
+    }
+
 }
---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Opcion.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.programacion.tallermecanico.vista;
+
+public enum Opcion {
+    INSERTAR_CLIENTE(11, "Insertar cliente."),
+    BUSCAR_CLIENTE(12, "Buscar cliente."),
+    BORRAR_CLIENTE(13, "Borrar cliente."),
+    LISTAR_CLIENTES(14, "Listar clientes."),
+    MODIFICAR_CLIENTE(15, "Modificar cliente."),
+    INSERTAR_VEHICULO(21, "Insertar vehiculo."),
+    BUSCAR_VEHICULO(22, "Buscar vehiculo")
+
+
+}
