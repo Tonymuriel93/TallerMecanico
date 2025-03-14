b3a24d9dadb6a242a0c31c2b693a89e967cb75c3
Tony Gomez Muriel <agmuriel93@gmail.com>
2025-03-14 14:24:21 +0100

    Commit inicial con el enunciado, las dependencias y los tests.

--- src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Vista.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Vista.java
@@ -1,7 +1,59 @@
 package org.iesalandalus.programacion.tallermecanico.vista;
 
+import org.iesalandalus.programacion.tallermecanico.modelo.TallerMecanicoExcepcion;
+
+import java.util.Objects;
+
 public class Vista {
     private Controlador controlador;
+
+    public void setControlador(Controlador controlador) {
+        Objects.requireNonNull(controlador, "ERROR: El controlador no puede ser nulo.");
+        this.controlador = controlador;
+    }
+
+    public void comenzar() {
+        Opcion opcion;
+        do {
+            Consola.mostrarMenu();
+            opcion = Consola.elegirOpcion();
+            ejecutar(opcion);
+        } while (opcion != Opcion.SALIR);
+        controlador.terminar();
+    }
+
+    public void terminar() {System.out.println("¡¡¡Hasta luego Lucasss!!!");}
+
+    private void ejecutar(Opcion opcion) {
+        try {
+            switch (opcion) {
+                case INSERTAR_CLIENTE -> insertarCliente();
+                case INSERTAR_VEHICULO -> insertarVehiculo();
+                case INSERTAR_REVISION -> insertarRevision();
+                case BUSCAR_CLIENTE -> buscarCliente();
+                case BUSCAR_VEHICULO -> buscarVehiculo();
+                case BUSCAR_REVISION -> buscarRevision();
+                case MODIFICAR_CLIENTE -> modificarCLiente();
+                case ANADIR_HORAS_REVISION -> anadirHoras();
+                case ANADIR_PRECIO_MATERIAL_REVISION -> anadirPrecioMaterial();
+                case CERRAR_REVISION -> cerrarRevision();
+                case BORRAR_CLIENTE -> borrarCliente();
+            }
+        } catch (Exception e) {
+            System.out.printf("ERROR: %s%n", e.getMessage());
+        }
+    }
+
+    private void insertarCliente() throws TallerMecanicoExcepcion {
+        Consola.mostrarCabecera("Insertar Cliente");
+        controlador.insertar(Consola.leerCliente());
+        System.out.println("Cliente insertado correctamente");
+    }
+
+    private void insertarVehiculo() throws TallerMecanicoExcepcion {
+        Consola.mostrarCabecera("Insertar Vehiculo");
+        controlador
+    }
 
 
 }
