64e18b589c25f27d58ddd16e86f88a6f63d61ec9
Tony Gomez Muriel <agmuriel93@gmail.com>
2025-03-18 14:35:07 +0100

    Commit inicial con el enunciado, las dependencias y los tests.

--- src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Vista.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Vista.java
@@ -2,8 +2,10 @@ package org.iesalandalus.programacion.tallermecanico.vista;
 
 import org.iesalandalus.programacion.tallermecanico.modelo.TallerMecanicoExcepcion;
 import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Revision;
 import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
 
+import java.util.List;
 import java.util.Objects;
 
 public class Vista {
@@ -73,7 +75,70 @@ public class Vista {
     private void buscarVehiculo() {
         Consola.mostrarCabecera("Buscar Vehículo");
         Vehiculo vehiculo = controlador.buscar(Consola.leerVehiculoMatricula());
+        System.out.println((vehiculo != null) ? vehiculo : "No existe ningún vehiculo con dicha matricula");
     }
+
+    private void buscarRevision() {
+        Consola.mostrarCabecera("Buscar Revisión");
+        Revision revision = controlador.buscar(Consola.leerRevision());
+        System.out.println((revision != null) ? revision : "No existe ninguna revisión para ese cliente, vehiculo y fecha.");
+    }
+
+    private void modificarCLiente() throws TallerMecanicoExcepcion {
+        Consola.mostrarCabecera("Modificar Cliente");
+        controlador.modificar(Consola.leerClienteDni(), Consola.leerNuevoNombre(), Consola.leerNuevoTelefono());
+        System.out.println("El cliente se ha modificado correctamente.");
+    }
+
+    private void anadirHoras() throws TallerMecanicoExcepcion {
+        Consola.mostrarCabecera("Añadir Horas Revisión");
+        controlador.anadirHoras(Consola.leerRevision(), consola.leerHoras());
+        System.out.println("Horas añadidas correctamente.");
+    }
+
+    private void anadirPrecioMaterial() throws TallerMecanicoExcepcion {
+        Consola.mostrarCabecera("Añadir Precio Material Revisión");
+        controlador.anadirPrecioMaterial(Consola.leerRevision(), Consola.leerFechaCierre());
+        System.out.println("Precio material añadido correctamente.");
+
+    }
+
+    private void cerrarRevision() throws TallerMecanicoExcepcion {
+        Consola.mostrarCabecera("Cerrar Revisión");
+        controlador.cerrar(Consola.leerRevision(), Consola.leerFechaCierre());
+        System.out.println("Revisión cerrada correctamente.");
+    }
+
+    private void borrarCliente() throws TallerMecanicoExcepcion {
+        Consola.mostrarCabecera("Borrar Cliente");
+        controlador.borrar(Consola.leerClienteDni());
+        System.out.println("Cliente borrado correctamente");
+    }
+
+    private void borrarVehiculo() throws TallerMecanicoExcepcion {
+        Consola.mostrarCabecera("Borrar Vehiculo");
+        controlador.borrar(Consola.leerVehiculoMatricula());
+        System.out.println("Vehiculo borrado correctamente");
+    }
+
+    private void borrarRevision() throws TallerMecanicoExcepcion {
+        Consola.mostrarCabecera("Borrar Revisión");
+        controlador.borrar(Consola.leerRevision());
+        System.out.println("Revisión borrado correctamente");
+}
+
+    private void listarClientes() {
+        Consola.mostrarCabecera("Listar Clientes");
+        List<Cliente> clientes = controlador.getClientes();
+        if (!clientes.isEmpty()) {
+            for (Cliente cliente : clientes) {
+                System.out.println(cliente);
+            }
+        } else {
+    }
+
+
+
 
 
 }
