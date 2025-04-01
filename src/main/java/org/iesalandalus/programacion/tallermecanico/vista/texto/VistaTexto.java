c2b19afe67eafa8a4711c02c9859dfafee048f96
Tony Gomez Muriel <agmuriel93@gmail.com>
2025-04-01 14:38:56 +0200

    Commit inicial con el enunciado, las dependencias y los tests.

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/texto/VistaTexto.java
@@ -0,0 +1,79 @@
+package org.iesalandalus.programacion.tallermecanico.vista.texto;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
+import org.iesalandalus.programacion.tallermecanico.vista.eventos.Evento;
+import org.iesalandalus.programacion.tallermecanico.vista.eventos.GestorEventos;
+
+public class VistaTexto implements Vista {
+
+    private final GestorEventos gestorEventos = new GestorEventos(Evento.values());
+
+    @Override
+    public GestorEventos getGestorEventos() {return gestorEventos;}
+
+    @Override
+    public void comenzar() {
+        Evento evento;
+        do {
+            Consola.mostrarMenu();
+            evento = Consola.elegirOpcion();
+            ejecutar(evento);
+        } while (evento != Evento.SALIR);
+    }
+
+    private void ejecutar(Evento evento) {
+        Consola.mostrarCabecera(evento.toString());
+        gestorEventos.notificar(evento);
+    }
+
+    @Override
+    public void terminar() {System.out.println("¡¡¡Hasta luego Lucasss!!!");}
+
+    @Override
+    public Cliente leerCliente() {
+        String nombre = Consola.leerCadena("Introduce el nombre: ");
+        String dni = Consola.leerCadena("Introduce el DNI: ");
+        String telefono = Consola.leerCadena("Introduce el teléfono:");
+        return new Cliente(nombre, dni, telefono);
+    }
+
+    @Override
+    public Cliente leerClienteDni() {return Cliente.get(Consola.leerCadena("Introduce el DNI: "));}
+
+    Override
+    public String leerNuevoNombre() {return Consola.leerCadena("Introduce el nuevo nombre: ");}
+
+    Override
+    public String leerNuevoTelefono() {return Consola.leerCadena("Introduce el nuevo teléfono: ");}
+
+    @Override
+    public Vehiculo leerVehiculo() {
+        String marca = Consola.leerCadena("Introduce la marca: ");
+        String modelo = Consola.leerCadena("Introduce el modelo:");
+        String matricula = Consola.leerCadena("Introduce la matricula: ");
+        return new Vehiculo(marca, modelo, matricula);
+    }
+
+
+    @Override
+    public Vehiculo leerVehiculoMatricula() {return }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
