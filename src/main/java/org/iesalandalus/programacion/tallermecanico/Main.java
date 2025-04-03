683f0f08c1bfc96e383b239f61977a0390537450
Tony Gomez Muriel <agmuriel93@gmail.com>
2025-04-03 14:25:41 +0200

    Commit inicial con el enunciado, las dependencias y los tests.

--- src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
@@ -1,7 +1,16 @@
 package org.iesalandalus.programacion.tallermecanico;
 
+import org.iesalandalus.programacion.tallermecanico.controlador.Controlador;
+import org.iesalandalus.programacion.tallermecanico.modelo.FabricaModelo;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.FabricaFuenteDatos;
+import org.iesalandalus.programacion.tallermecanico.vista.FabricaVista;
+import org.iesalandalus.programacion.tallermecanico.vista.Modelo;
+
 public class Main {
     public static void main(String[] args) {
-
+        Modelo modelo = FabricaModelo.CASCADA.crear(FabricaFuenteDatos.MEMORIA);
+        Vista vista = FabricaVista.TEXTO.crear();
+        Controlador controlador = new Controlador(modelo,vista);
+        controlador.comenzar();
     }
 }
--- src/main/java/org/iesalandalus/programacion/tallermecanico/vista/texto/VistaTexto.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/texto/VistaTexto.java
@@ -1,19 +1,20 @@
 package org.iesalandalus.programacion.tallermecanico.vista.texto;
 
-import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
-import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Trabajo;
-import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.*;
 import org.iesalandalus.programacion.tallermecanico.vista.eventos.Evento;
 import org.iesalandalus.programacion.tallermecanico.vista.eventos.GestorEventos;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class VistaTexto implements Vista {
 
     private final GestorEventos gestorEventos = new GestorEventos(Evento.values());
 
     @Override
-    public GestorEventos getGestorEventos() {return gestorEventos;}
+    public GestorEventos getGestorEventos() {
+        return gestorEventos;
+    }
 
     @Override
     public void comenzar() {
@@ -31,7 +32,9 @@ public class VistaTexto implements Vista {
     }
 
     @Override
-    public void terminar() {System.out.println("¡¡¡Hasta luego Lucasss!!!");}
+    public void terminar() {
+        System.out.println("¡¡¡Hasta luego Lucasss!!!");
+    }
 
     @Override
     public Cliente leerCliente() {
@@ -42,13 +45,21 @@ public class VistaTexto implements Vista {
     }
 
     @Override
-    public Cliente leerClienteDni() {return Cliente.get(Consola.leerCadena("Introduce el DNI: "));}
+    public Cliente leerClienteDni() {
+        return Cliente.get(Consola.leerCadena("Introduce el DNI: "));
+    }
 
     Override
-    public String leerNuevoNombre() {return Consola.leerCadena("Introduce el nuevo nombre: ");}
+
+    public String leerNuevoNombre() {
+        return Consola.leerCadena("Introduce el nuevo nombre: ");
+    }
 
     Override
-    public String leerNuevoTelefono() {return Consola.leerCadena("Introduce el nuevo teléfono: ");}
+
+    public String leerNuevoTelefono() {
+        return Consola.leerCadena("Introduce el nuevo teléfono: ");
+    }
 
     @Override
     public Vehiculo leerVehiculo() {
@@ -60,31 +71,148 @@ public class VistaTexto implements Vista {
 
 
     @Override
-    public Vehiculo leerVehiculoMatricula() {return Vehiculo.get(Consola.leerCadena("Introduce la matricula: "));}
+    public Vehiculo leerVehiculoMatricula() {
+        return Vehiculo.get(Consola.leerCadena("Introduce la matricula: "));
+    }
 
     @Override
     public Trabajo leerRevision() {
         Cliente cliente = leerClienteDni();
         Vehiculo vehiculo = leerVehiculoMatricula();
         LocalDate fechaInicio = Consola.leerFecha("Introduce la fecha de inicio");
-        re
+        return new Revision(cliente, vehiculo, fechaInicio);
     }
 
+    @Override
+    public Trabajo leerMecanico() {
+        Cliente cliente = leerClienteDni();
+        Vehiculo vehiculo = leerVehiculoMatricula();
+        LocalDate fechaInicio = Consola.leerFecha("Introduce la fecha de inicio");
+        return new Mecanico(cliente, vehiculo, fechaInicio);
+    }
 
+    @Override
+    public Trabajo leerTrabajoVehiculo() {
+        return Trabajo.get(leerVehiculoMatricula());
+    }
 
+    @Override
+    public int leerHoras() {
+        return Consola.leerEntero("Introduce las horas a añadir");
+    }
 
+    @Override
+    public float leerPrecioMaterial() {
+        return Consola.leerReal("Introduce el precio del material a añadir: ");
+    }
 
+    @Override
+    public LocalDate leerFechaCierre() {
+        return Consola.leerFecha("Introduce la fecha de cierre");
+    }
 
+    @Override
+    public void notificarResultado(Evento evento, String texto, boolean exito) {
+        if (exito) {
+            System.out.println(texto);
+        } else {
+            System.out.printf("ERROR: %s%n", texto);
+        }
+    }
 
+    @Override
+    public void mostrarCliente(Cliente cliente) {
+        System.out.println((cliente != null) ? cliente : "No existe ningún cliente con dicho DNI.");
+    }
 
+    @Override
+    public void mostrarVehiculo(Vehiculo vehiculo) {
+        System.out.println((vehiculo != null) ? vehiculo : "No existe ningún vehiculo con dicha matrícula.");
+    }
 
+    @Override
+    public void mostrarTrabajo(Trabajo trabajo) {
+        System.out.println((trabajo != null) ? trabajo : "No existe ningún trabajo para ese cliente, vehiculo y fecha.");
+    }
 
+    @Override
+    public void mostrarClientes(List<Cliente> clientes) {
+        if (!clientes.isEmpty()) {
+            for (Cliente cliente : clientes) {
+                System.out.println(cliente);
+            }
+        } else {
+            System.out.println("No hay clientes que mostrar.");
+        }
+    }
 
+    @Override
+    public void mostrarVehiculos(List<Vehiculo> vehiculos) {
+        if (!vehiculos.isEmpty()) {
+            for (Vehiculo vehiculo : vehiculos) {
+                System.out.println(vehiculo);
+            }
+        } else {
+            System.out.println("No hay vehículos que mostrar.");
+        }
+    }
 
+    @Override
+    public void mostrarTrabajos(List<Trabajo> trabajos) {
+        if (!trabajos.isEmpty()) {
+            for (Trabajo trabajo : trabajos) {
+                System.out.println(trabajo);
+            }
+        } else {
+            System.out.println("No hay trabajos que mostrar.");
+        }
+    }
 
+    @Override
+    public void mostrarTrabajosCliente(List<Trabajo> trabajosCliente) {
+        if (!trabajosCliente.isEmpty()) {
+            for (Trabajo trabajo : trabajosCliente) {
+                System.out.println(trabajo);
+            }
+        } else {
+            System.out.println("No hay trabajos que mostrar para dicho cliente.");
+        }
+    }
 
-
-
-
-
+    @Override
+    public void mostrarTrabajosVehiculo(List<Trabajo> trabajosVehiculo) {
+        if (!trabajosVehiculo.isEmpty()) {
+            for (Trabajo trabajo : trabajosVehiculo) {
+                System.out.println(trabajo);
+            }
+        } else {
+            System.out.println("No hay trabajos que mostrar para dicho vehiculo.");
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
